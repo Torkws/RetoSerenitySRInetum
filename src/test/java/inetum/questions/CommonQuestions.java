6817f978d0b9a1ae1d5c51995d94d0d969f5cbc9
Torkws <47335736+Torkws@users.noreply.github.com>
2026-01-29 13:46:00 -0500

    update: definicion questions

---
+++ src/test/java/inetum/questions/CommonQuestions.java
@@ -0,0 +1,79 @@
+package inetum.questions;
+
+import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.targets.Target;
+import net.serenitybdd.screenplay.Actor;
+
+import static net.serenitybdd.screenplay.questions.WebElementQuestion.the;
+
+public class CommonQuestions implements Question<Boolean> {
+
+    private String questionDescription;
+
+    private CommonQuestions(String description) {
+        this.questionDescription = description;
+    }
+
+    @Override
+    public Boolean answeredBy(Actor actor) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return questionDescription;
+    }
+
+    public static Question<Boolean> checkIfObjectIsDisplayed(Target target) {
+        return Question.about("✓ Verificar que el elemento está visible: " + target.getName())
+                .answeredBy(actor -> {
+                    try {
+                        return the(target)
+                                .answeredBy(actor)
+                                .isVisible();
+                    } catch (Exception e) {
+                        return false;
+                    }
+                });
+    }
+
+    public static Question<Boolean> checkIfObjectIsDisplayed(Target target, String message) {
+        return Question.about("✓ " + message)
+                .answeredBy(actor -> {
+                    try {
+                        return the(target)
+                                .answeredBy(actor)
+                                .isVisible();
+                    } catch (Exception e) {
+                        return false;
+                    }
+                });
+    }
+
+    public static Question<String> getObjectText(Target target) {
+        return Question.about("✓ Obtener el texto del elemento: " + target.getName())
+                .answeredBy(actor -> {
+                    try {
+                        return the(target)
+                                .answeredBy(actor)
+                                .getText();
+                    } catch (Exception e) {
+                        return "";
+                    }
+                });
+    }
+
+
+    public static Question<String> getObjectText(Target target, String message) {
+        return Question.about("✓ " + message)
+                .answeredBy(actor -> {
+                    try {
+                        return the(target)
+                                .answeredBy(actor)
+                                .getText();
+                    } catch (Exception e) {
+                        return "";
+                    }
+                });
+    }
+}
--- src/test/java/inetum/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/inetum/stepdefinitions/LoginStepDefinitions.java
@@ -1,11 +1,13 @@
 package inetum.stepdefinitions;
 
+import inetum.questions.CommonQuestions;
 import inetum.tasks.IniciarSesionTask;
+import inetum.ui.CommonPage;
+import inetum.ui.InventoyPage;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import net.serenitybdd.screenplay.actors.OnStage;
-
-
+import net.serenitybdd.screenplay.ensure.Ensure;
 
 public class LoginStepDefinitions {
 
@@ -18,14 +20,35 @@ public class LoginStepDefinitions {
 
     @Then("el usuario debería ser redirigido a la página de productos")
     public void el_usuario_debería_ser_redirigido_a_la_página_de_productos() {
+        OnStage.theActorInTheSpotlight().attemptsTo(
+                Ensure.that(CommonQuestions.checkIfObjectIsDisplayed(
+                        InventoyPage.INVENTORY_PAGE_TITLE,
+                        "El usuario fue redirigido a la página de productos - Validando título"
+                ))
+                        .isTrue()
+        );
     }
 
     @Then("el inventario de productos debería ser visible")
     public void el_inventario_de_productos_debería_ser_visible() {
+        OnStage.theActorInTheSpotlight().attemptsTo(
+                Ensure.that(CommonQuestions.checkIfObjectIsDisplayed(
+                        InventoyPage.PRODUCTS_INVENTORY,
+                        "El inventario de productos es visible en la página"
+                ))
+                        .isTrue()
+        );
     }
 
     @Then("^se debería mostrar un mensaje de error de login (.*)")
     public void se_debería_mostrar_un_mensaje_de_error_de_login(String messageError) {
+        OnStage.theActorInTheSpotlight().attemptsTo(
+                Ensure.that(CommonQuestions.getObjectText(
+                        CommonPage.ERROR_MESSAGE_LOGIN_CSS,
+                        "Validando que se muestra el mensaje de error esperado: " + messageError
+                ))
+                        .contains(messageError)
+        );
     }
 
 
